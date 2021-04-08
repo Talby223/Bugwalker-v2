47d0994eb70804c631e15d8950ac8125d4ebdb20
Talby223 <generaldov@gmail.com>
2021-04-08 11:36:39 -0500

    restrict comments to logged-in users

--- Web/FrontController.hs
+++ Web/FrontController.hs
@@ -5,6 +5,7 @@ import Web.Controller.Prelude
 import Web.View.Layout (defaultLayout)
 
 -- Controller Imports
+import Web.Controller.Comments
 import Web.Controller.Users
 import Web.Controller.Specs
 import Web.Controller.Static
@@ -18,6 +19,7 @@ instance FrontController WebApplication where
     controllers = 
         [ startPage WelcomeAction
         -- Generator Marker
+        , parseRoute @CommentsController
         , parseRoute @UsersController
         , parseRoute @SpecsController
         , parseRoute @SessionsController
